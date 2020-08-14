54e5b476fb12da7e2d3f4a08086ed430ab04c1b7
Михаил <meekesp@gmail.com>
2020-08-14 14:06:02 +0300

    parse Api privateBank

---
+++ simpleCode/differntProjects/Parsing/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Parsing {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.CausesValidation = false;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(31, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(361, 219);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "getCourse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ simpleCode/differntProjects/Parsing/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsing {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            label1.Text = "exchange rate privat bank\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (richTextBox1.Text!= "")
+                richTextBox1.Text = "";
+
+            string line = "";
+
+            using (WebClient wc = new WebClient()) {
+                line = wc.DownloadString("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5");
+                Match match = Regex.Match(line, "<exchangerate ccy=\"USD\" base_ccy=\"UAH\" buy=\"(.*?)\" sale=\"(.*?)\"/>");
+                richTextBox1.Text += "US:   buy - " + match.Groups[1].Value + "  sale - " + match.Groups[2];
+                match = Regex.Match(line, "<exchangerate ccy=\"EUR\" base_ccy=\"UAH\" buy=\"(.*?)\" sale=\"(.*?)\"/>");
+                richTextBox1.Text += "\nEUR: buy - " + match.Groups[1].Value + "  sale - " + match.Groups[2];
+
+                line = wc.DownloadString("https://api.myip.com");
+                match = Regex.Match(line, "{\"ip\":\"(.*?)\",\"country\":\"(.*?)\",\"cc\":\"(.*?)\"}");
+
+                richTextBox1.Text += "\n";
+
+                richTextBox1.Text += "ip " + match.Groups[1].Value + "  " + match.Groups[2].Value + "\n\n";
+                line = wc.DownloadString("http://worldtimeapi.org/api/ip");
+                //"2020-08-13T15:25:55.591251+03:00",
+                match = Regex.Match(line, "\"datetime\":\"(.*?)\",");
+                line = match.Groups[1].Value;
+                richTextBox1.Text += line.Substring(0, 10)+"\n";
+                richTextBox1.Text += line.Substring(11, 8);
+            }
+        }
+    }
+}
