088ea285e8977935503633d469460953587e868e
Михаил <meekesp@gmail.com>
2020-08-25 19:33:24 +0300

    add project greayscale

---
+++ simpleCode/differntProjects/BlackAndWhiteImg/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BlackAndWhiteImg {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOnpen = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnGrayScale = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox2_Resize);
+            // 
+            // btnOnpen
+            // 
+            this.btnOnpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOnpen.Location = new System.Drawing.Point(445, 409);
+            this.btnOnpen.Name = "btnOnpen";
+            this.btnOnpen.Size = new System.Drawing.Size(94, 29);
+            this.btnOnpen.TabIndex = 1;
+            this.btnOnpen.Text = "Open file";
+            this.btnOnpen.UseVisualStyleBackColor = true;
+            this.btnOnpen.Click += new System.EventHandler(this.btnOnpen_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(414, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(374, 391);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Resize += new System.EventHandler(this.pictureBox2_Resize);
+            // 
+            // btnGrayScale
+            // 
+            this.btnGrayScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGrayScale.Location = new System.Drawing.Point(565, 409);
+            this.btnGrayScale.Name = "btnGrayScale";
+            this.btnGrayScale.Size = new System.Drawing.Size(94, 29);
+            this.btnGrayScale.TabIndex = 3;
+            this.btnGrayScale.Text = "Grayscale";
+            this.btnGrayScale.UseVisualStyleBackColor = true;
+            this.btnGrayScale.Click += new System.EventHandler(this.btnGrayScale_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(694, 409);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGrayScale);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnOnpen);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOnpen;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnGrayScale;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ simpleCode/differntProjects/BlackAndWhiteImg/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BlackAndWhiteImg {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Resize(object sender, EventArgs e) {
+
+        }
+
+        private void btnOnpen_Click(object sender, EventArgs e) {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image File (*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if(fileDialog.ShowDialog() == DialogResult.OK) {
+                try {
+                    pictureBox1.Image = new Bitmap(fileDialog.FileName);
+                }
+                catch {
+                    MessageBox.Show("Can't open this file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnGrayScale_Click(object sender, EventArgs e) {
+            if(pictureBox1.Image != null ) {
+                Bitmap input = new Bitmap(pictureBox1.Image);
+                Bitmap output = new Bitmap(input.Width, input.Height);
+
+                for (int i = 0; i < input.Height; i++) {
+                    for (int y = 0; y < input.Width; y++) {
+                        uint pixel = (uint)(input.GetPixel(y,i).ToArgb());
+
+                        float Alfa = (pixel & 0xff000000) >> 24;
+                        float R = (pixel & 0x00ff0000) >> 16;
+                        float G = (pixel & 0x0000ff00) >> 8;
+                        float B =  (pixel & 0x000000ff);
+
+                        R = G = B = (R + G + B) / 3.0f;
+
+                        uint npixel = 0x000000|(uint)Alfa<<24 | ((uint)R << 16) | ((uint)G << 8) | (uint)B;
+
+                        output.SetPixel(y,i, Color.FromArgb((int)npixel));
+                    }
+                }
+                pictureBox2.Image = output;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            if(pictureBox2 != null) {
+                SaveFileDialog savefile = new SaveFileDialog();
+                savefile.Title = "Save image as...";
+                savefile.OverwritePrompt = true;
+                //savefile.CheckFileExists = true;
+                savefile.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+                if (savefile.ShowDialog() == DialogResult.OK) {
+                    try {
+                        pictureBox2.Image.Save(savefile.FileName);
+                    }
+                    catch {
+                        MessageBox.Show("Can't save this file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
